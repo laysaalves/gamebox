879bd630a31ef339692b3aade6f307245454573d
Layseiras <layseirasdev@outlook.com>
2025-03-20 16:02:57 -0300

    initial commit

---
+++ src/main/java/dev/layseiras/gamebox/GameboxApplication.java
@@ -0,0 +1,13 @@
+package dev.layseiras.gamebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/layseiras/gamebox/GameboxApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.layseiras.gamebox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
